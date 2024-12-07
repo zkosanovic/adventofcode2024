cd44a5c681af75f569f5bcc219f094e78061200b
Zdravko Kosanovic <zdravkokg@mail.ru>
2024-12-08 00:38:11 +0100

    Day 3 - Part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::io;
 
 fn main() {
-    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)|(do)(\(\))|(don)'t(\(\))").unwrap();
 
     let mut input = String::new();
 
@@ -14,9 +14,24 @@ fn main() {
     }
 
     let mut result: u64 = 0;
+    let mut on = true;
 
     for cap in re.captures_iter(&input) {
-        let (_, nums): (&str, [&str; 2]) = cap.extract();
+        let (s, nums): (&str, [&str; 2]) = cap.extract();
+
+        if s == "don't()" {
+            on = false;
+            continue;
+        }
+
+        if s == "do()" {
+            on = true;
+            continue;
+        }
+
+        if !on {
+            continue;
+        }
 
         let x = nums[0].parse::<u64>().unwrap();
         let y = nums[1].parse::<u64>().unwrap();
