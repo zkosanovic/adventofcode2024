b5b9472d9efb4baebcfcbdb8da082d0ed10ed98c
Zdravko Kosanovic <zdravkokg@mail.ru>
2024-12-08 01:59:05 +0100

    Day 4 - Part 2

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -23,39 +23,15 @@ fn main() {
 
     for (i, row) in mat.iter().enumerate() {
         for (j, cell) in row.iter().enumerate() {
-            if *cell != 'X' {
+            if *cell != 'A' || i < 1 || j < 1 || i >= n - 1 || j >= m - 1 {
                 continue;
             }
 
-            if i < n - 3 && mat[i+1][j] == 'M' && mat[i+2][j] == 'A' && mat[i+3][j] == 'S' {
-                result += 1;
-            }
-
-            if i > 2 && mat[i-1][j] == 'M' && mat[i-2][j] == 'A' && mat[i-3][j] == 'S' {
-                result += 1;
-            }
-
-            if j < m - 3 && mat[i][j+1] == 'M' && mat[i][j+2] == 'A' && mat[i][j+3] == 'S' {
-                result += 1;
-            }
-
-            if j > 2 && mat[i][j-1] == 'M' && mat[i][j-2] == 'A' && mat[i][j-3] == 'S' {
-                result += 1;
-            }
-
-            if i > 2 && j > 2 && mat[i-1][j-1] == 'M' && mat[i-2][j-2] == 'A' && mat[i-3][j-3] == 'S' {
-                result += 1;
-            }
-
-            if i > 2 && j < m - 3 && mat[i-1][j+1] == 'M' && mat[i-2][j+2] == 'A' && mat[i-3][j+3] == 'S' {
-                result += 1;
-            }
-
-            if i < n - 3 && j > 2 && mat[i+1][j-1] == 'M' && mat[i+2][j-2] == 'A' && mat[i+3][j-3] == 'S' {
-                result += 1;
-            }
-
-            if i < n - 3 && j < m - 3 && mat[i+1][j+1] == 'M' && mat[i+2][j+2] == 'A' && mat[i+3][j+3] == 'S' {
+            if ((mat[i - 1][j - 1] == 'M' && mat[i + 1][j + 1] == 'S')
+                || (mat[i - 1][j - 1] == 'S' && mat[i + 1][j + 1] == 'M'))
+                && ((mat[i - 1][j + 1] == 'M' && mat[i + 1][j - 1] == 'S')
+                    || (mat[i - 1][j + 1] == 'S' && mat[i + 1][j - 1] == 'M'))
+            {
                 result += 1;
             }
         }
